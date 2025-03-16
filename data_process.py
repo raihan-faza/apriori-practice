45161d9156757f0fc060228884363c18e9757c61
lahh <raihanfazasabiqr@gmail.com>
2025-03-16 14:58:05 +0700

    cleaning data

--- data_process.py
+++ data_process.py
@@ -1,6 +1,18 @@
 import pandas as pd
 
+# explore dataset
 df = pd.read_csv("online_retail.csv")
-print(df.info)
+print(df.info())
 print(df.describe)
 print(df.isna().sum())
+
+# cleaning dataset
+## removing extra space
+df["Description"] = df["Description"].str.strip()
+print(df["Description"])
+
+## drop null/nan values
+df.dropna(inplace=True)
+print(df.info())
+
+# encoding data
